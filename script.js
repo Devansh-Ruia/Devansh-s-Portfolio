7b7da696f5663f5b465c01438971424ac37375cd
digigran <ruiadevansh@gmail.com>
2024-09-16 14:54:54 +0530

    hi

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Form submission handling
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Message sent! Now, I’ll get back to you once I done killing this Sekiro boss.');
+    alert('Message sent! Now, I’ll get back to you once I beat this Sekiro boss.');
 });
+
 // Initialize AOS library for scroll animations
 AOS.init({
     duration: 1000, // Animation duration in milliseconds
     once: true // Animation happens only once
 });
+
 // Scroll-triggered animations for headings
 window.addEventListener('scroll', function() {
     const headings = document.querySelectorAll('h2');
