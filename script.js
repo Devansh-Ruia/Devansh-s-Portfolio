6b1fead0b761cd95f5011a8e231e240f0cb9bb57
digigran <ruiadevansh@gmail.com>
2024-09-17 15:16:37 +0530

    hi

--- script.js
+++ script.js
@@ -37,3 +37,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
